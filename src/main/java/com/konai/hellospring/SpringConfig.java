e5494e779e26fa8ee1ef567f5aa20b3c23796d7d
suzy-thetraveler <suzythetraveler@khu.ac.kr>
2022-01-04 22:16:07 +0900

    [spring practice 21/01/04]

--- src/main/java/com/konai/hellospring/SpringConfig.java
+++ src/main/java/com/konai/hellospring/SpringConfig.java
@@ -1,13 +1,22 @@
 package com.konai.hellospring;
 
+import com.konai.hellospring.repository.JdbcMemberRepository;
 import com.konai.hellospring.repository.MemberRepository;
 import com.konai.hellospring.repository.MemoryMemberRepository;
 import com.konai.hellospring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,7 +25,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
